2ab937511a3d46ee22b2c2849406a53704e04fb6
AMB <a.michael.boyle@gmail.com>
2021-09-09 18:16:03 -0400

    server base file set up

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 8080
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
+app.use("/auth", require("./routes/auth"));
+app.use("/api", require("./routes/api"));
+
 app.listen(PORT, function () {
     console.log(`Server running on port: ${PORT})`)
 })
