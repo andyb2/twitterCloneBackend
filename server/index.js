acb602e833df2a1af2ba1c7a31c591f8ca4ed780
AMB <a.michael.boyle@gmail.com>
2021-09-09 19:31:51 -0400

    no change

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 require('dotenv');
 const express = require('express');
 const app = express();
+const session = require("express-session");
 const PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
-
-app.use("/auth", require("./routes/auth"));
-app.use("/api", require("./routes/api"));
 
 app.listen(PORT, function () {
     console.log(`Server running on port: ${PORT})`)
