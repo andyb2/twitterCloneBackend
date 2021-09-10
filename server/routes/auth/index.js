d506fee9825c281cd81f0f8bf5d40154bed295f9
AMB <a.michael.boyle@gmail.com>
2021-09-10 17:03:02 -0400

    line 49 const pwIsValid added to await the results of comparing the password to hash. line 70 route for logout created

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,58 +1,74 @@
 const router = require("express").Router();
-const { User } = require("../../db/models");
+const { User } = require("../../db/models/");
 const jwt = require("jsonwebtoken");
 
 router.post('/register', async (req, res, next) => {
-    const { username, password, email } = req.body
-    if (!username || !email || !password) {
-        return res.status(400).json({ error: "Username, password and email are required" })
-    }
-    if (password.length < 8) {
-        return res.status(400).json({ error: "Password must be at least 8 characters" })
-    }
-
-    const registerData = {
-        username: username,
-        email: email,
-        password: password
-    }
-
-    const registerUser = await User.create(registerData)
-    if (!registerUser.id) {
-        console.log({ error: `Sorry failed to create entry for ${registerUser.username}` })
-        res.status(401).json({ error: `Sorry failed to create entry for ${registerUser.username}` })
-    }
-
-    return {
-        status: true,
-        message: `Success! ${registerUser.username} was successfully registered`,
-        savedUserData: {
-            id: registerUser.id,
-            username: registerUser.username,
-            email: registerUser.email,
+    try {
+        const { username, password, email } = req.body
+        if (!username || !email || !password) {
+            return res.status(400).json({ error: "Username, password and email are required" })
         }
+        if (password.length < 6) {
+            return res.status(400).json({ error: "Password must be at least 8 characters" })
+        }
+
+        const registerData = {
+            username: username,
+            email: email,
+            password: password
+        }
+
+        const registerUser = await User.create(registerData)
+        if (!registerUser.id) {
+            console.log({ error: `Sorry failed to create entry for ${registerUser.username}` })
+            res.status(401).json({ error: `Sorry failed to create entry for ${registerUser.username}` })
+        }
+        res.sendStatus(201)
+        // return {
+        //     status: true,
+        //     message: `Success! ${registerUser.username} was successfully registered`,
+        //     savedUserData: {
+        //         id: registerUser.id,
+        //         username: registerUser.username,
+        //         email: registerUser.email,
+        //     }
+        // }
+    } catch (error) {
+        next(error)
     }
 });
 
 router.post('/login', async (req, res, next) => {
-    const { username, password } = req.body
-    const userData = await User.findOne({
-        where: {
-            username: req.body.username
+    try {
+        const { username, password } = req.body
+        const userData = await User.findOne({
+            where: {
+                username: req.body.username
+            }
+        });
+        const pwIsValid = await userData.checkCorrectPassword(password, userData.password());
+
+        if (!userData) {
+            console.log({ error: `${username} was not found` })
+            res.status(401).json({ error: `${username} was not found` })
+        } else if (pwIsValid === false) {
+            console.log({ error: `Wrong username or password` })
+            res.status(401).json({ error: `Wrong username or password` })
+        } else {
+            const token = jwt.sign(
+                { id: userData.dataValues.id },
+                process.env.SESSION_SECRET,
+                { expiresIn: 86400 }
+            );
+            res.status(201).json({ ...userData, token })
         }
-    });
-    if (!userData) {
-        console.log({ error: `${username} was not found` })
-        res.status(401).json({ error: `${username} was not found` })
-    } else if (!userData.checkCorrectPassword(password, userData.password())) {
-        console.log({ error: `Wrong username or password` })
-        res.status(401).json({ error: `Wrong username or password` })
-    } else {
-        const token = jwt.sign(
-            { id: userData.dataValues.id },
-            process.env.SESSION_SECRET,
-            { expiresIn: 86400 }
-        );
-        res.json({ ...userData, token })
+    } catch (error) {
+        next(error)
     }
+});
+
+router.delete('/logout', (req, res, next) => {
+    res.sendStatus(204)
 })
+
+module.exports = router
