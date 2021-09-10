cf0ec1853931879e9a1008f52c029c01204222a0
AMB <a.michael.boyle@gmail.com>
2021-09-09 20:27:41 -0400

    User  model created with username email and password

---
+++ server/db/models/user.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const User = db.define("user", {
+    username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        alloWNull: false,
+        validate: { isEmail: true }
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { min: 8 },
+    }
+});
+
+module.exports = User
