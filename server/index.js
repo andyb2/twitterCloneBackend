c2e520bbfc56cf2eaae9fb1f12de41ab28ed0e39
AMB <a.michael.boyle@gmail.com>
2021-09-10 17:00:03 -0400

    moved express contents to out of index to app.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,5 @@
-require('dotenv');
-const express = require('express');
-const session = require("express-session");
+const app = require('./app')
 const PORT = process.env.PORT || 8080
-const { join } = require("path");
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
-app.use(express.static(join(__dirname, "public")));
-
-app.use("/auth", require("./routes/auth"));
 
 app.listen(PORT, function () {
     console.log(`Server running on port: ${PORT}`)
